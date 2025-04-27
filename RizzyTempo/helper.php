af7f136d778b62d2263ae4153b84360101bca9a5
ThingWei <chngtw-pm23@student.tarc.edu.my>
2025-04-27 15:19:23 +0800

    Update helper.php

--- RizzyTempo/helper.php
+++ RizzyTempo/helper.php
@@ -5,7 +5,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 
-define("DB_HOST", "44.203.36.140");
+define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "music_society");
