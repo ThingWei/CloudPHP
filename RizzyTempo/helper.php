55a9a5a416f3f78ce6ad7fa527d7055822a97928
ThingWei <chngtw-pm23@student.tarc.edu.my>
2025-04-27 16:15:51 +0800

    db password: secure_password

--- RizzyTempo/helper.php
+++ RizzyTempo/helper.php
@@ -7,7 +7,7 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "secure_password");
 define("DB_NAME", "music_society");
 
 function checkEventName($eventname) {
