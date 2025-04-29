7074df0613d32457a7d7ca60f1237f5c5c5203c3
ThingWei <chngtw-pm23@student.tarc.edu.my>
2025-04-29 14:32:20 +0800

    Update cart.php

--- RizzyTempo/cart.php
+++ RizzyTempo/cart.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_email'])) {
     exit();
 }
 
-$con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
