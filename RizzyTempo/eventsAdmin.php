2de9948c975906f58cbfe0727bc05e9b15313d34
ThingWei <chngtw-pm23@student.tarc.edu.my>
2025-04-29 14:37:31 +0800

    Update eventsAdmin.php

--- RizzyTempo/eventsAdmin.php
+++ RizzyTempo/eventsAdmin.php
@@ -41,7 +41,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Events</title>
+    <title>Products</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="css/header.css" rel="stylesheet">
